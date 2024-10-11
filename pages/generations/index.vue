d45053337005bf895d485aaa1639ab3e2569ddfe
Rafael <rafaelforato5@gmail.com>
2024-10-11 01:28:16 -0300

    first commit

---
+++ pages/generations/index.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { Generation } from '~/entities/Generation';
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth'],
+});
+
+const { loading, generations, isEmpty } = useGenerations();
+const { loadings, download, fetchAudioSignedUrl } = useAudioSignedUrl();
+
+const handleDownload = async ({
+  title,
+  audioId,
+}: Pick<Generation, 'title' | 'audioId'>) => {
+  await fetchAudioSignedUrl(audioId);
+  download(title);
+};
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col items-center gap-10 p-5 pb-32">
+    <Onboarding
+      title="Olá, suas gerações aparecerão aqui!"
+      guide="Você pode gerar uma nova pelo botão no topo da página."
+      class="w-full max-w-4xl"
+    />
+
+    <div v-if="loading" class="w-full max-w-4xl flex flex-col gap-2">
+      <Skeleton v-for="n in 5" :key="n" height="50px" />
+    </div>
+
+    <div v-if="isEmpty && !loading" class="w-full max-w-4xl flex gap-2">
+      <Card class="w-full h-32 flex justify-center items-center">
+        <template #content>
+          <p class="text-base text-gray-700">
+            <i class="pi pi-exclamation-triangle" />
+            Nenhuma geração encontrada.
+          </p>
+        </template>
+      </Card>
+    </div>
+
+    <div
+      v-if="!isEmpty && !loading"
+      class="w-full max-w-4xl flex flex-col gap-2"
+    >
+      <p class="text-base text-gray-700">
+        {{ generations.length }}
+        <span v-if="generations.length === 1">áudio encontrado.</span>
+        <span v-else>áudios encontrados.</span>
+      </p>
+
+      <Generation
+        v-for="generation in generations"
+        :key="generation.id"
+        :title="generation.title"
+        :content="generation.content"
+        :audio-id="generation.audioId"
+        :date="generation.createdAt"
+        :is-downloading="loadings[generation.audioId]"
+        @download="handleDownload"
+      />
+    </div>
+  </div>
+</template>
